5b24e25320632c93481ad0403cfe61b4fa6e9549
mishamyrt <misha@myrt.co>
2019-12-03 14:54:31 +0300

    Improved fish complition

--- completions/mysh.fish
+++ completions/mysh.fish
@@ -2,30 +2,37 @@
 
 set -l hosts_subcmds show
 
-# version
-complete -c mysh -f -n '__fish_use_subcommand' -a version -d 'Display version and exit'
-complete -c mysh -f -n '__fish_seen_subcommand_from version'
+# get
+complete -c mysh -f -n '__fish_use_subcommand' -a get -d 'add repository and download hosts from it'
+complete -c mysh -f -n '__fish_seen_subcommand_from get'
 
-# remotes
-complete -c mysh -f -n '__fish_use_subcommand' -a remotes -d 'Display all added remote repositories'
-complete -c mysh -f -n '__fish_seen_subcommand_from remotes'
-
-# update
-complete -c mysh -f -n '__fish_use_subcommand' -a update -d 'Update hosts from all added remote repositories'
-complete -c mysh -f -n '__fish_seen_subcommand_from update'
-
-# namespaces
-complete -c mysh -f -n '__fish_use_subcommand' -a namespaces -d 'Display all namespaces'
-complete -c mysh -f -n '__fish_seen_subcommand_from namespaces'
+# help
+complete -c mysh -f -n '__fish_use_subcommand' -a help -d 'print help message and exit'
+complete -c mysh -f -n '__fish_seen_subcommand_from help'
 
 # hosts
-complete -c mysh -f -n '__fish_use_subcommand' -a hosts -d 'Display all hosts'
+complete -c mysh -f -n '__fish_use_subcommand' -a hosts -d 'display all hosts'
 complete -c mysh -f -n '__fish_seen_subcommand_from hosts'
 
+# namespaces
+complete -c mysh -f -n '__fish_use_subcommand' -a namespaces -d 'display all namespaces'
+complete -c mysh -f -n '__fish_seen_subcommand_from namespaces'
+
+# remotes
+complete -c mysh -f -n '__fish_use_subcommand' -a remotes -d 'display all added remote repositories'
+complete -c mysh -f -n '__fish_seen_subcommand_from remotes'
+
 # show
-complete -c mysh -f -n '__fish_use_subcommand' -a show -d 'Shows host information'
+complete -c mysh -f -n '__fish_use_subcommand' -a show -d 'display host information'
 complete -c mysh -f -n '__fish_seen_subcommand_from show'
 
+# update
+complete -c mysh -f -n '__fish_use_subcommand' -a update -d 'refresh hosts from added remote repositories'
+complete -c mysh -f -n '__fish_seen_subcommand_from update'
+
+# version
+complete -c mysh -f -n '__fish_use_subcommand' -a version -d 'print Mysh version'
+complete -c mysh -f -n '__fish_seen_subcommand_from version'
 
 complete -c mysh  -n "__fish_seen_subcommand_from $hosts_subcmds" -a (cat $HOME/.local/share/mysh/completion)
 complete -c mysh -f -n '__fish_use_subcommand' -a (cat $HOME/.local/share/mysh/completion)
