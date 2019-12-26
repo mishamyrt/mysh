cf739b9502a1a8837249daaca7864d1cd9d96db7
mishamyrt <misha@myrt.co>
2019-12-27 01:00:17 +0300

    Fixed version information

--- mysh.go
+++ mysh.go
@@ -2,6 +2,15 @@ package main
 
 import "github.com/mishamyrt/mysh/v1/cmd"
 
+// GitCommit refers to commit hash at the moment of build
+var GitCommit string
+
+// Version of Mysh
+var Version string
+
+// BuildTime of Mysh
+var BuildTime string
+
 func main() {
 	cmd.Execute()
 }
